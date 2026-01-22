ab86b8a1d175d421715b076492ce61700c50f43f
SorenG1 <gom23015@byui.edu>
2026-01-21 18:28:21 -0700

    Sorry, I finally got everything sorted out

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+    
+    {   
+        string response = "yes";
+        do
+        {
+       Random randomGenerator = new Random ();
+       int number = randomGenerator.Next(0,101);
+
+        int num_guess = -1;
+
+
+        while (num_guess != number)
+        
+        {
+        Console.WriteLine("Take a guess: ");
+        num_guess = int.Parse(Console.ReadLine());
+
+        
+        if (num_guess == number)
+        {
+            Console.WriteLine ("You got it! ");
+        }
+        if (num_guess > number)
+        {
+            Console.WriteLine ("Try a little lower man...");
+        }
+        if (num_guess < number)
+
+        {
+            Console.WriteLine ("GO up!");
+        }
+
+        
+        }
+        Console.WriteLine("Do you want to play again? ");
+        response = Console.ReadLine();
+
+        }while (response == "yes");
     }
 }
+    }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,65 @@
 using System;
-
+using System.Collections.Generic;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+    int number = 0;
+    int sum = 0;
+
+
+    List <int> numbers = new List<int> (); 
+    Console.WriteLine("Enter a list of Numbers, type 0 when you're done. ");
+
+        do
+        {
+        Console.WriteLine("Enter number: ");
+        number = int.Parse(Console.ReadLine());
+        if (number != 0)
+        {
+        numbers.Add(number);  
+        }
+        }while (number != 0);
+    
+    foreach (int n in numbers)
+        {
+            sum += n;
+        }
+    Console.WriteLine($"The sum of this list is: {sum}");
+
+    Console.WriteLine($"The number of elements in the list is {numbers.Count}.");
+
+    double average = (double)sum / numbers.Count ();
+
+    Console.WriteLine($"The average is: {average}.");
+    
+    int largest = numbers[0];
+
+    foreach (int n in numbers)
+    {
+        if (n > largest)
+        {
+            largest = n;
+        }
     }
+    Console.WriteLine($"The largest number in the list is: {largest}. ");
+
+    int smallest = numbers[0];
+
+    foreach (int n in numbers)
+    {
+        if (n < smallest)
+        {
+            smallest = n;
+        }
+    }
+    Console.WriteLine($"The smallest number in the list is: {smallest}. ");
+
+    numbers.Sort();
+    foreach (int n in numbers)
+    {
+        Console.WriteLine(n);
+    }
+    }
+
 }
